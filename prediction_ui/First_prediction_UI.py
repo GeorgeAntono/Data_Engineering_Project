d13fccf3e282fa98291f8204a9aa624ed55f765a
GeorgeAntono <georgantonopoulos@hotmail.com>
2023-10-17 13:58:13 +0200

    Changing Prediction UI to be in-line with lab2 standards.

--- prediction_ui/First_prediction_UI.py
+++ prediction_ui/First_prediction_UI.py
@@ -1,7 +1,13 @@
+# importing Flask and other modules
+import json
+import os
+import logging
+import requests
 from flask import Flask, request, render_template, jsonify
 
 # Flask constructor
 app = Flask(__name__)
+
 
 
 # A decorator used to tell the application
@@ -12,17 +18,31 @@ def check_churn():
         return render_template("input_form_page.html")
 
     elif request.method == "POST":
-        CreditScore = int(request.form.get("CreditScore"))  # getting input with name = ntp in HTML form
-        Geography = int(request.form.get("Geography"))  # getting input with name = pgc in HTML form
-        Gender = int(request.form.get("Gender"))
-        Age = int(request.form.get("Age"))
-        Tenure = int(request.form.get("Tenure"))
-        Balance = float(request.form.get("Balance"))
-        NumOfProducts = float(request.form.get("NumOfProducts"))
-        HasCrCard = int(request.form.get("HasCrCard"))
-        IsActiveMember = int(request.form.get("IsActiveMember"))
-        EstimatedSalary = int(request.form.get("EstimatedSalary"))
+        prediction_input = [
+            {
+        "CreditScore": int(request.form.get("CreditScore")),  # getting input with name = ntp in HTML form
+        "Geography": int(request.form.get("Geography")),  # getting input with name = pgc in HTML form
+        "Gender": int(request.form.get("Gender")),
+        "Age": int(request.form.get("Age")),
+        "Tenure": int(request.form.get("Tenure")),
+        "Balance": float(request.form.get("Balance")),
+        "NumOfProducts": float(request.form.get("NumOfProducts")),
+        "HasCrCard": int(request.form.get("HasCrCard")),
+        "IsActiveMember": int(request.form.get("IsActiveMember")),
+        "EstimatedSalary": int(request.form.get("EstimatedSalary"))
+            }
+                            ]
 
+        logging.debug("Prediction input : %s", prediction_input)
+
+        predictor_api_url = os.environ['PREDICTOR_API']
+
+        res = requests.post(predictor_api_url, json=json.loads(json.dumps(prediction_input)))
+
+        prediction_value = res.json()['result']
+        logging.info("Prediction Output : %s", prediction_value)
+        return render_template("response_page.html",
+                               prediction_variable=prediction_value)
         # we will replace this simple (and inaccurate logic) with a prediction from a machine learning model in a
         # future in a future lab
         if EstimatedSalary > 12000:
@@ -33,13 +53,15 @@ def check_churn():
         return render_template("response_page.html",
                                prediction_variable=prediction_value)
 
+
     else:
+
         return jsonify(message="Method Not Allowed"), 405  # The 405 Method Not Allowed should be used to indicate
-    # that our app that does not allow the users to perform any other HTTP method (e.g., PUT and  DELETE) for
-    # '/checkdiabetes' path
+        # that our app that does not allow the users to perform any other HTTP method (e.g., PUT and  DELETE) for
+        # /checkchurn
 
 
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
