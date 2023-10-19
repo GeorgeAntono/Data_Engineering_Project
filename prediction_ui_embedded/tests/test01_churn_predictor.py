b1c66b78577bf86752fcfb6181e302f34b227822
Lars van Rijckevorsel <larsvanrijckevorsel2001@gmail.com>
2023-10-19 16:31:53 +0200

    Added the prediction_request json file

--- prediction_ui_embedded/tests/test01_churn_predictor.py
+++ prediction_ui_embedded/tests/test01_churn_predictor.py
@@ -8,10 +8,10 @@ import churn_predictor
 class TestChurnPredictor:
     def test_predict_single_record(self):
         test_dir = os.path.dirname(os.path.abspath(__file__))
-        test_data_file = os.path.join(test_dir, "../testResources/churn.csv")
+        test_data_file = os.path.join(test_dir, "../testResources/prediction_request.json")
         test_model_file = os.path.join(test_dir, "../testResources/model.pkl")
-        with open(test_data_file) as csv_file:
-            data = pd.read_csv(csv_file)
+        with open(test_data_file) as json_file:
+            data = pd.read_json(json_file)
         cp = churn_predictor.ChurnPredictor(model_file=test_model_file)
         status = cp.predict_single_record(data)
        
