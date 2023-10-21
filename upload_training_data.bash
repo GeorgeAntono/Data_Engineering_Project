d070846cc012622df956fe2a4c2482854dd5f23e
GeorgeAntono <georgantonopoulos@hotmail.com>
2023-10-21 20:50:57 +0200

    Corrected echo path

--- upload_training_data.bash
+++ upload_training_data.bash
@@ -2,7 +2,7 @@ gsutil cp ../data/* gs://data_degroup11
 
 git config --global user.email "georgantonopoulos@hotmail.com"
 git config --global user.name "GeorgeAntono"
-echo "data uploaded" >> data/history.txt
+echo "data uploaded" >> synchronizer/pipeline/history.txt
 git commit -am "data uploaded"
 git push https://$1:$2@github.com/GeorgeAntono/DE_Group11.git --all
 
