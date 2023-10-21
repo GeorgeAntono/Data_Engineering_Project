158ddbdeb3e81e1dbbf20548f24f779165e293a2
GeorgeAntono <georgantonopoulos@hotmail.com>
2023-10-21 19:33:38 +0200

    creating bash script to trigger our pipeline execution

---
+++ upload_training_data.bash
@@ -0,0 +1,8 @@
+gsutil cp ../data/* gs://degroup11
+
+git config --global user.email "georgantonopoulos@hotmail.com"
+git config --global user.name "GeorgeAntono"
+echo "data uploaded" >> data/history.txt
+git commit -am "data uploaded"
+git push https://$1:$2@github.com/GeorgeAntono/DE_Group11.git --all
+
