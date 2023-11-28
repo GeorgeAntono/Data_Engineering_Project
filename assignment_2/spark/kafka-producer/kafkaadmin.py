10d03750231e28d5bb1a81189687305ae09a5444
GeorgeAntono <georgantonopoulos@hotmail.com>
2023-11-29 00:48:57 +0100

    Assigment 2 - started stream and kafka. Everything is still experimental.

---
+++ assignment_2/spark/kafka-producer/kafkaadmin.py
@@ -0,0 +1,18 @@
+from kafka.admin import KafkaAdminClient, NewTopic
+
+
+def delete_topics(admin):
+    admin.delete_topics(topics=['mock'])
+    admin.delete_topics(topics=['mock_2'])
+
+
+def create_topics(admin, topic_list):
+    admin.create_topics(new_topics=topic_list, validate_only=False)
+
+
+if __name__ == '__main__':
+    admin_client = KafkaAdminClient(bootstrap_servers="35.225.176.195:9092",
+                                    client_id='spark')  # use your VM's external IP Here!
+    topic_list = [NewTopic(name="mock", num_partitions=1, replication_factor=1),
+                  NewTopic(name="mock_2", num_partitions=1, replication_factor=1)]
+    create_topics(admin_client, topic_list)
